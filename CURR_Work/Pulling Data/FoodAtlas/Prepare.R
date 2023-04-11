62560ab667430bf002a84d1d2a6d1f964037b436
John Malla <74744334+mallaja5708@users.noreply.github.com>
2023-04-11 18:34:24 -0400

    Initial Commit for 4.11.23

--- CURR_Work/Pulling Data/FoodAtlas/Prepare.R
+++ CURR_Work/Pulling Data/FoodAtlas/Prepare.R
@@ -1,4 +1,5 @@
 library(tidyverse)
+library(tidycensus)
 foodAccess <- read_xlsx("FoodAccessResearchAtlasData2015.xlsx", sheet = 2)
 
 vaFoodAccess <- FoodAccess %>% filter(State == "Virginia")
@@ -15,4 +16,99 @@ counties <- c("Chesapeake City", "Franklin City", "Hampton City",
 
 vaCoFoodAccess <- vaFoodAccess %>% filter(County %in% counties)
 
+#<!----------------------------------------------------------------->
+#<!----------------- Getting Hyesoo's Variables--------------------->
+#<!----------------------------------------------------------------->
+
+# setwd("TheHamptonGapfinders/CURR_Work/Pulling Data/FoodAtlas")
+
+my.df <- read.csv("countyAtlasData.csv")
+
+string <- "CensusTract
+State
+County
+Urban
+OHU2010
+PCTGQTRS
+LILATracts_1And10
+LILATracts_halfAnd10
+LILATracts_1And20
+LILATracts_Vehicle
+LowIncomeTracts
+PovertyRate
+LA1and10
+LAhalfand10
+LA1and20
+LATracts_half
+LATracts1
+LATracts10
+LATracts20
+LATractsVehicle_20
+LAPOP1_10
+LAPOP05_10
+LAPOP1_20
+LALOWI1_10
+LALOWI05_10
+LALOWI1_20
+lapophalf
+lalowihalf
+laseniorshalf
+laseniorshalfshare
+lablackhalf
+lablackhalfshare
+laasianhalfshare
+lahunvhalf
+lahunvhalfshare
+lasnaphalf
+lasnaphalfshare
+lapop1
+lapop1share
+lalowi1
+lalowi1share
+laseniors1
+laseniors1share
+lablack1
+lablack1share
+lahunv1
+lahunv1share
+lasnap1
+lasnap1share
+lalowi10
+lalowi10share
+laseniors10
+laseniors10share
+lablack10
+lablack10share
+lahunv10
+lahunv10share
+lasnap10
+lasnap10share
+lapop20
+lapop20share
+lalowi20
+lalowi20share
+lablack20
+lablack20share
+lahunv20
+lahunv20share
+lasnap20
+lasnap20share
+TractLOWI
+TractSeniors
+TractBlack
+TractHUNV"
+
+string.split <- unlist(strsplit(string, "\n"))
+
+new.df <- my.df %>% select(string.split)
+
+write.csv(x = new.df, "FIN_Atlas_CountyData.csv")
+
+
+county_fips <- c(550, 620, 650, 700, 710, 735, 740, 800, 810,
+                 830, 073, 093, 095, 115, 175, 199)
+temp <- get_acs(county = county_fips, state = "VA", 
+                geography = "tract",
+                variables = "S1901_C01_012", 
+                year = 2015, geometry = TRUE,)
 
--- CURR_Work/TravelTime.R
+++ CURR_Work/TravelTime.R
@@ -1,1 +1,51 @@
 install.packages("traveltimeR")
+
+library(traveltimeR)
+
+Sys.setenv(TRAVELTIME_ID = "acfb52eb")
+Sys.setenv(TRAVELTIME_KEY = "eb02516c05b7d10f8e03477312b537fa")
+
+dateTime <- strftime(as.POSIXlt(Sys.time(), "UTC"), "%Y-%m-%dT%H:%M:%SZ")
+
+library(traveltimeR)
+
+Sys.setenv(TRAVELTIME_ID = "YOUR_APP_ID")
+Sys.setenv(TRAVELTIME_KEY = "YOUR_APP_KEY")
+
+locations <- c(
+  make_location(
+    id = 'London center',
+    coords = list(lat = 51.508930, lng = -0.131387)),
+  make_location(
+    id = 'Hyde Park',
+    coords = list(lat = 51.508824, lng = -0.167093)),
+  make_location(
+    id = 'ZSL London Zoo',
+    coords = list(lat = 51.536067, lng = -0.153596))
+)
+
+departure_search <-
+  make_search(id = "departure search example",
+              departure_location_id = "London center",
+              arrival_location_ids = list("Hyde Park", "ZSL London Zoo"),
+              departure_time = strftime(as.POSIXlt(Sys.time(), "UTC"), "%Y-%m-%dT%H:%M:%SZ"),
+              properties = list("travel_time", "distance", "route"),
+              transportation = list(type = "driving"))
+
+arrival_search <-
+  make_search(id = "arrival  search example",
+              arrival_location_id = "London center",
+              departure_location_ids = list("Hyde Park", "ZSL London Zoo"),
+              arrival_time = strftime(as.POSIXlt(Sys.time(), "UTC"), "%Y-%m-%dT%H:%M:%SZ"),
+              properties = list('travel_time', "distance", "route", "fares"),
+              transportation = list(type = "public_transport"),
+              range = list(enabled = T, width = 1800, max_results = 1))
+
+result <-
+  routes(
+    departure_searches = departure_search,
+    arrival_searches = arrival_search,
+    locations = locations
+  )
+
+print(result)
