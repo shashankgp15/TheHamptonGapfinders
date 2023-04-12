78f839b2e38d42aade93f823fbd40e384ff489c5
John Malla <74744334+mallaja5708@users.noreply.github.com>
2023-04-12 19:45:39 -0400

    Final Prepare, uploading shape file

--- CURR_Work/Pulling Data/FoodAtlas/Prepare.R
+++ CURR_Work/Pulling Data/FoodAtlas/Prepare.R
@@ -1,7 +1,7 @@
 library(tidyverse)
-library(tidycensus)
 library(readxl)
 setwd("CURR_Work/Pulling Data/FoodAtlas/")
+rm(list = ls())
 
 #<!----------------------------------------------------->
 #<- This block of code is just for the Prepare.R 
@@ -20,16 +20,22 @@ masterFoodAccess <- rbind(foodAccess15, foodAccess19)
 
 
 foodAccess_Va <- masterFoodAccess %>% filter(State == "Virginia")
-counties <- c("Chesapeake City", "Franklin City", "Hampton City", 
-              "Newport News City", "Norfolk City", "Poquoson City",
-              "Portsmouth City", "Suffolk City", "Virginia Beach City",
-              "Williamsburg City", "Gloucester County", "Isle of Wight County",
-              "James City County", "Mathews County", "Southampton County", "York County",
-              "Chesapeake", "Franklin", "Hampton", 
-              "Newport News", "Norfolk", "Poquoson",
-              "Portsmouth", "Suffolk", "Virginia Beach",
-              "Williamsburg", "Gloucester", "Isle of Wight",
-              "James", "Mathews", "Southampton", "York")
+counties <- c("Chesapeake", "Chesapeake city", 
+              "Franklin city", "Franklin City", 
+              "Hampton", "Hampton city",
+              "Newport News", "Newport News city",
+              "Norfolk", "Norfolk city",
+              "Poquoson", "Poquoson city", 
+              "Portsmouth", "Portsmouth city",
+              "Suffolk", "Suffolk city",
+              "Virginia Beach", "Virginia Beach city",
+              "Williamsburg", "Williamsburg city",
+              "Gloucester", "Gloucester County",
+              "Isle of Wight", "Isle of Wight County",
+              "James City", "James City County",
+              "Mathews", "Mathews County",
+              "Southampton", "Southampton County",
+              "York", "York County")
 
 foodAccess_VaTr <- foodAccess_Va %>% filter(County %in% counties)
 
@@ -41,6 +47,7 @@ desiredColumns <- "CensusTract
 State
 County
 Urban
+year
 OHU2010
 PCTGQTRS
 LILATracts_1And10
@@ -112,10 +119,62 @@ TractBlack
 TractHUNV"
 
 desiredColumns <- unlist(strsplit(desiredColumns, "\n")) 
-# strsplit searching for new line character int he string above
 
-vatr_foodAccess_FIN <- foodAccess_VaTr %>% select(desiredColumns, -"")
+vatr_foodAccess_FIN <- foodAccess_VaTr %>% select(desiredColumns)
 
 
-# write.csv(x = vatr_foodAccess_FIN, "Working/vatr_foodAccess.csv")
+# write.csv(x = vatr_foodAccess_FIN, "Working/vatr_foodAccess.csv", row.names = FALSE)
 # Commented out because not needed after first is saved
+
+
+#<!---------------------------------------------------------------->
+#< Trying to figure out how to get polygon / shape into file
+#<!---------------------------------------------------------------->
+
+rm(list = ls())
+setwd("CURR_Work/Pulling Data/FoodAtlas")
+library(tidyverse)
+library(tidycensus)
+library(sf)
+library(sp)
+
+
+foodAccess <- read.csv("Working/vatr_foodAccess.csv")
+
+hamptonGeoid <- unique(substr(foodAccess$CensusTract, 1, 5))
+
+countyOutlines <- county_laea %>% filter(GEOID %in% hamptonGeoid)
+
+acsGeoBase <- get_acs(geography = "tract", variables = "B19013_001",
+                state = "VA", geometry = TRUE, year = 2019)
+
+useable <- acsGeoBase %>% select(GEOID, NAME, geometry)
+useableHampton <- useable %>% filter(substr(GEOID, 1, 5) %in% hamptonGeoid)
+
+geometry <- NULL
+for(i in 1:nrow(foodAccess)){
+  geometry <- rbind(geometry, st_as_sf(data.frame(useableHampton[useableHampton$GEOID == foodAccess$CensusTract[i], "geometry"])))
+}
+
+
+#<!---------------------------------------------------------------->
+#< we have geometry in order, and food Access, we need to merge
+#< so that foodAccess is an sf and contians MULTIPOLYGON data
+#<!---------------------------------------------------------------->
+foodAccess.sf <- NULL
+for(i in 1:nrow(foodAccess)){
+  foodAccess.sf <- rbind(foodAccess.sf, st_as_sf(merge(foodAccess[i,], geometry[i,])))
+}
+
+
+#<!---------------------------------------------------------------->
+#<!-                      Example case                        ----->
+#<!---------------------------------------------------------------->
+
+
+example <- foodAccess.sf %>% select(lapophalf)
+example$lapophalf <- as.double(example$lapophalf)
+plot(example)
+
+
+
